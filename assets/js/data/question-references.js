75fb838bd5ace03f4529b9ee823cefbcf5d57c29
Mohammad <moeltaher@users.noreply.github.com>
2026-08-22 01:44:33 +0300

    Align question references with primary authority

--- assets/js/data/question-references.js
+++ assets/js/data/question-references.js
@@ -1,7 +1,7 @@
 import {authorityLeaders} from './authority-model.js';
 
 const label={worker:'العامل',platform:'المنصة',client:'العميل',mediator:'الوسيط'};
-function leaderAnswers(type,axis){const leaders=authorityLeaders(type,axis);if(leaders.length>1){const answers=['سلطة مشتركة بين المنصة والعميل'];leaders.forEach(p=>answers.push(label[p]));return answers}return leaders.map(p=>label[p])}
+function leaderAnswers(type,axis){return authorityLeaders(type,axis).map(p=>label[p])}
 export const questionRef=Object.fromEntries(['data','moderation','ai','translation'].map(type=>[type,{
  price:leaderAnswers(type,'price'),
  allocation:leaderAnswers(type,'allocation'),
