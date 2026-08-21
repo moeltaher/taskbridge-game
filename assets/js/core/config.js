73bb6076cdeb0b5298d9f1697cd4795d6ff202c9
Mohammad <moeltaher@users.noreply.github.com>
2026-08-21 19:32:31 +0300

    Remove legacy model version constants

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -1,5 +1,3 @@
 export const APP_NAME='No Boss';
 export const APP_VERSION='3.0.2';
 export const RESULT_VERSION=`${APP_NAME} v${APP_VERSION}`;
-export const SCORING_VERSION=6;
-export const TIME_MODEL_VERSION=2;
