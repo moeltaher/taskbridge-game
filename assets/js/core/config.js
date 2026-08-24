ab5d22b5fced8f5ce98d2dd98ae62716ab427bb1
Mohammad <moeltaher@users.noreply.github.com>
2026-08-24 23:36:42 +0300

    keep app shell version stable while advancing learning models

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -1,5 +1,5 @@
 export const APP_NAME='No Boss';
-export const APP_VERSION='4.0.0';
+export const APP_VERSION='3.8.0';
 export const SCORE_MODEL_VERSION='9';
 export const ECONOMY_MODEL_VERSION='4';
 export const RESULT_VERSION=`${APP_NAME} v${APP_VERSION}`;
